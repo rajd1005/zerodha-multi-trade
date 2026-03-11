728c6954c157a70390fcdc5a829ee2a9e343d3e0
rajd1005 <rajdey.dey1@gmail.com>
2026-03-12 02:59:09 +0530

    Update admin.js

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,8 +3,13 @@ const router = express.Router();
 const { getAllUsers, updateUserStatus, adminCreateUser } = require('../controllers/adminCtrl');
 const { protect, admin } = require('../middleware/authMiddleware');
 
+// Get all registered users
 router.get('/users', protect, admin, getAllUsers);
+
+// Update a specific user's subscription or status
 router.put('/users/:id', protect, admin, updateUserStatus);
+
+// Manually add a new user
 router.post('/users', protect, admin, adminCreateUser);
 
 module.exports = router;
