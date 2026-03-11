a69e42990ad2a1dd4780b37c744c75c39255b284
rajd1005 <rajdey.dey1@gmail.com>
2026-03-12 01:07:41 +0530

    Update notification.js

--- src/routes/notification.js
+++ src/routes/notification.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { subscribe } = require('../controllers/notificationCtrl');
+const { subscribe, getPublicKey } = require('../controllers/notificationCtrl');
 const { protect } = require('../middleware/authMiddleware');
 
+// Public route for frontend to get the key
+router.get('/vapid-public-key', getPublicKey);
+
+// Protected route to save the user's device subscription
 router.post('/subscribe', protect, subscribe);
 
 module.exports = router;
