a4fe007a943732597eef7641bfda51629f024c8e
rajd1005 <rajdey.dey1@gmail.com>
2026-03-11 23:52:40 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const connectDB = require('./config/db');
 const tradeRoutes = require('./routes/trade');
 const authRoutes = require('./routes/auth'); // Add this line
 const brokerRoutes = require('./routes/broker');
+const adminRoutes = require('./routes/admin'); // Add this near the top
 
 const app = express();
 
@@ -17,6 +18,7 @@ connectDB();
 app.use('/api/trades', tradeRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/brokers', brokerRoutes); // Add this line
+app.use('/api/admin', adminRoutes); // Add this line
 
 app.get('/health', (req, res) => res.status(200).send('Server is healthy & running!'));
 
