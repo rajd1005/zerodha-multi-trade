ae3deb62f8243d576a202ed5af6d980b199b5b49
rajd1005 <rajdey.dey1@gmail.com>
2026-03-12 00:19:00 +0530

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/authCtrl');
+const { registerUser, loginUser, forgotPassword, resetPassword } = require('../controllers/authCtrl');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.post('/forgotpassword', forgotPassword);
+router.put('/resetpassword/:resettoken', resetPassword);
 
 module.exports = router;
