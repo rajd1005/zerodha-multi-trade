f89125fd4a0226b738ba9c940bb750aaaf4c04bb
rajd1005 <rajdey.dey1@gmail.com>
2026-03-12 14:04:37 +0530

    Update data.js

--- src/routes/data.js
+++ src/routes/data.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getBalance, getPositions, searchInstruments } = require('../controllers/dataCtrl');
+const { getBalance, getPositions, searchInstruments, getLtp, getOptionChain } = require('../controllers/dataCtrl');
 const { protect } = require('../middleware/authMiddleware');
 
 router.get('/balance', protect, getBalance);
 router.get('/positions', protect, getPositions);
 router.get('/search', protect, searchInstruments);
+
+// New routes for fetching Live LTP and Option Chain
+router.get('/ltp', protect, getLtp);
+router.get('/option-chain', protect, getOptionChain);
 
 module.exports = router;
