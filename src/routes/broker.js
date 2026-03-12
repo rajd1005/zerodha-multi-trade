465fca59b934b2e2a108972471dd52fca72e266f
rajd1005 <rajdey.dey1@gmail.com>
2026-03-12 13:08:10 +0530

    Update broker.js

--- src/routes/broker.js
+++ src/routes/broker.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { addBrokerAccount, getBrokerAccounts, updateAccessToken } = require('../controllers/brokerCtrl');
+const { 
+    addBrokerAccount, 
+    getBrokerAccounts, 
+    updateAccessToken, 
+    generateAccessToken 
+} = require('../controllers/brokerCtrl');
 const { protect } = require('../middleware/authMiddleware');
 
 // All routes here are protected
 router.route('/').post(protect, addBrokerAccount).get(protect, getBrokerAccounts);
+
+// Manual daily token update (Fallback/Legacy method)
 router.route('/:id/token').put(protect, updateAccessToken);
+
+// Automated OAuth Login: Exchange request_token for access_token
+router.route('/generate-session').post(protect, generateAccessToken);
 
 module.exports = router;
